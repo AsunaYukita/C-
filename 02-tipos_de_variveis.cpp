be540b6e875e53636e209a51ad8e1c5328cbc749
henrique <henriquepatricio70@gmail.com>
2023-04-03 17:27:39 -0300

    first commit

---
+++ 02-tipos_de_variveis.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    system ("cls");
+   cout<< "O que sao variaveis?";
+ int  valorUnitario = 10;
+   float valorEmGrama = 2.5;
+   double salario = 2500.50;
+   char letra = 'a';
+   string  nome = "alexandre";
+    bool palmeirasnaotemmundial = true;
+    return 0;
+}
